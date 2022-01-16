26c87a56164da45a5638ccd4b7f6239a8af09466
Schumann <hc_schumann@hotmail.com>
2022-01-16 16:43:26 -0300

    Implement player rotation and final movement behaviour

--- Angry Asteroids/Assets/Scripts/PlayerController.cs
+++ Angry Asteroids/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,28 +5,47 @@ public class PlayerController : MonoBehaviour
 {
     public float PlayerMovementSpeed = 10f;
 
-    private Vector3 _currentInput;
+    private bool _moveForward;
+    private Camera _mainCamera;
 
-    void Start()
+    private void Start()
     {
-        
+        _mainCamera = Camera.main;
     }
 
     void Update()
     {
-        transform.position += _currentInput * Time.deltaTime * PlayerMovementSpeed;
+        if(_moveForward)
+        {
+            transform.position += transform.right * Time.deltaTime * PlayerMovementSpeed;
+        }
     }
 
     public void OnPlayerMoved(InputAction.CallbackContext context)
     {
         if(context.performed)
         {
-            var playerInput = context.ReadValue<Vector2>();
-            _currentInput = new Vector3(playerInput.x, playerInput.y, 0);
+            _moveForward = true;
         }
         else if(context.canceled)
         {
-            _currentInput = Vector3.zero;
+            _moveForward = false;
+        }
+    }
+
+    public void OnMouseMoved(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            var pointerPosition = context.ReadValue<Vector2>();
+            var pointerWorldPosition = _mainCamera.ScreenToWorldPoint(new Vector3(pointerPosition.x, pointerPosition.y, 0));
+            var pointerWorld2DPosition = new Vector2(pointerWorldPosition.x, pointerWorldPosition.y);
+            
+            var playere2DPosition = new Vector2(transform.position.x, transform.position.y);
+            var pointerToPlayerVector = pointerWorld2DPosition - playere2DPosition;
+            var newRight = new Vector3(pointerToPlayerVector.x, pointerToPlayerVector.y, 0);
+            
+            transform.right = newRight.normalized;
         }
     }
 }
